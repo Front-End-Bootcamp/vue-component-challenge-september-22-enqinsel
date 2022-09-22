603c0290385bd561e4740b9434caf63cf4bbf52b
enqinsel <enqinsel@gmail.com>
2022-09-22 22:16:17 +0300

    added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,59 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonsComp from "./components/ButtonsComp.vue"
+import DATA from "../public/data.json"
+
+function clickHandler() {
+    let list = document.getElementById("list")
+    let li = document.createElement("li")
+  
+    function filterByGroups(data) {
+  
+      const groups = data.reduce((currentData, person) => {
+        currentData[person.group] = []
+        return currentData
+      }, {})
+  
+  
+      Object.keys(groups).forEach(group => {
+        const students = data.filter(person => person.group == group)
+        groups[group] = students
+      })
+      return groups
+    }
+  
+    const groups = filterByGroups(DATA)
+    li.innerHTML = JSON.stringify(groups.Orchid)
+    list.appendChild(li)
+  
+    return;
+  }
+
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <ButtonsComp groupName="Orchid" @action="clickHandler"></ButtonsComp>
+    <ButtonsComp groupName="SteelBlue"></ButtonsComp>
+    <ButtonsComp groupName="IndianRed"></ButtonsComp>
+    <ButtonsComp groupName="LightSeaGreen"></ButtonsComp>
+    <ButtonsComp groupName="YellowGreen"></ButtonsComp>
+  </div>
+
+  <div class="students">
+    <ul id="list">
+
+    </ul>
+  </div>
 </template>
 
+
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
+.students {
+  position: fixed;
+  width: auto;
+  height: auto;
+  top: 350px;
+  left: 150px;
 }
 
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/ButtonsComp.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const props = defineProps(["groupName"])
+const emits = defineEmits(["action"])
+const clickHandler = () => emits("action")
+</script>
+
+<template>
+    <div>
+        <h1>{{props.groupName}}</h1>
+        <button @click="clickHandler">Select</button>
+    </div>
+</template>
+
+<style scoped>
+div{
+    width: 250px;
+    height: 200px;
+    float: left;
+    margin: 10px;
+    padding: 10px;
+    background-color: aqua;
+    border: 2px solid red;
+}
+
+h1{
+    width: 25px;
+    height: auto;
+    margin-left: 35px;
+}
+
+button{
+    width: 80px;
+    height: 50px;
+    background-color: bisque;
+    margin-left: 70px;
+    margin-top: 40px;
+}
+</style>
